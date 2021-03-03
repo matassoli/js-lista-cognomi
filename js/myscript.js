9974e6e3d1f9e7ad04f878a1c88bf59110e1a9bc
Nicolò Matassoli <nico.matassoli@gmail.com>
2021-03-03 17:48:43 +0100

    initial

---
+++ js/myscript.js
@@ -0,0 +1,13 @@
+ var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+
+
+ var cognomeUtente = prompt("Qual è il tuo cognome?");
+ cognomi.push(cognomeUtente);
+ cognomi.sort();
+ console.log(cognomi);
+
+
+
+
+
+ // scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
