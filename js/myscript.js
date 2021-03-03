d953514eedb70a4be5d8f9447b8f1bf7beb14595
Nicolò Matassoli <nico.matassoli@gmail.com>
2021-03-03 18:37:28 +0100

    cognome uppercase

--- js/myscript.js
+++ js/myscript.js
@@ -1,13 +1,11 @@
  var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
 
  var cognomeUtente = prompt("Qual è il tuo cognome?");
- cognomi.push(cognomeUtente);
+ var cognomeCap = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.slice(1);
+ cognomi.push(cognomeCap);
  cognomi.sort();
-
-
-
  for (var i = 0; i < cognomi.length; i++) {
    document.getElementById("cognomiNum").innerHTML += "<li>" + cognomi[i] + "</li>";
  }
-var cognomePos = parseInt(cognomi.indexOf(cognomeUtente));
+ var cognomePos = parseInt(cognomi.indexOf(cognomeCap));
  document.getElementById("cognomePos").innerHTML = cognomePos + 1;
