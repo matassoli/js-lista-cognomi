1f978c2d7bf098b232db8eda9c84c6e1e5f7b967
Nicolò Matassoli <nico.matassoli@gmail.com>
2021-03-04 10:39:51 +0100

    polish2

--- js/myscript.js
+++ js/myscript.js
@@ -1,17 +1,17 @@
  var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"],
-   lettere = /^[a-zA-Z]+$/;
+   lettere = /^[a-zA-Z ]+$/;
 
  do {
    var cognomeUtente = prompt("Qual è il tuo cognome?");
  }
  while (!cognomeUtente.match(lettere));
 
- var cognomeCap = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.slice(1).toLowerCase();
- cognomi.push(cognomeCap);
+ var cognomeUtente = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.slice(1).toLowerCase();
+ cognomi.push(cognomeUtente);
  cognomi.sort();
 
  for (var i = 0; i < cognomi.length; i++) {
    document.getElementById("cognomiNum").innerHTML += "<li>" + cognomi[i] + "</li>";
  }
- var cognomePos = cognomi.indexOf(cognomeCap);
+ var cognomePos = cognomi.indexOf(cognomeUtente);
  document.getElementById("cognomePos").innerHTML = cognomePos + 1;
